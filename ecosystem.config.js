538298b62898c0d7297bc11e0da462fb050ea50a
pjhpoi66 <60897503+pjhpoi66@users.noreply.github.com>
2025-10-05 12:24:45 +0900

    c

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: 'resource_server', // 앱 이름
+            script: 'npm',
+            args: 'start',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
+};
